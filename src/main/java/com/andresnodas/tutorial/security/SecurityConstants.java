3c605f0f7c7a2828e6c2dd8f65a9854a68d2cb79
Andres Nodas <nodasiniesta@gmail.com>
2021-01-24 23:59:56 +0700

    Making REST Stateless, Reading Token Secret from a property file

--- src/main/java/com/andresnodas/tutorial/security/SecurityConstants.java
+++ src/main/java/com/andresnodas/tutorial/security/SecurityConstants.java
@@ -1,11 +1,18 @@
 package com.andresnodas.tutorial.security;
 
+import com.andresnodas.tutorial.SpringApplicationContext;
+
 public class SecurityConstants {
 
 	public static final long EXPIRATION_TIME = 864000000;
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGNUP_URL = "/users";
-	public static final String TOKEN_SECRET = "secretkey";
+	
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+		
+		return appProperties.getTokenSecret();
+	}
 	
 }
--- src/main/java/com/andresnodas/tutorial/security/WebSecurity.java
+++ src/main/java/com/andresnodas/tutorial/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.andresnodas.tutorial.service.UserService;
@@ -24,13 +25,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
 		.antMatchers(HttpMethod.POST, SecurityConstants.SIGNUP_URL).permitAll()
-		.anyRequest().authenticated()
-		.and().addFilter(new AuthenticationFilter(authenticationManager()));
+		.anyRequest().authenticated().and()
+		.addFilter(getAuthenticationFilter())
+		.addFilter(new AuthorizationFilter(authenticationManager()))
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	private AuthenticationFilter getAuthenticationFilter() throws Exception {
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		
+		return filter;
 	}
 	
 }
